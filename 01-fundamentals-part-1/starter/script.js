4ede047ca8036190de332c01c0036060e82be0e0
Sophia Leigh <ahorrosophialeigh@gmail.com>
2025-08-20 13:04:21 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,61 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+let js ="amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("GUMANA KA");
+
+console.log("=== VARIABLES ===");
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+console.log(age); 
+age = 50;
+console.log(age);
+
+const birthYear = 2005;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "programmer";
+job = "designer";
+console.log(job); 
+
+let lastName = "Ahorro"
+
+console.log("=== DATA TYPES ===");
+
+//number
+age = 20;
+console.log(age);
+console.log(typeof age);
+
+//string
+let username = "iya";
+console.log(username);
+console.log(typeof username)
+
+//boolean
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun)
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+//dynamic variable
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//change to string
+dynamicVariable = "Now I'm a string";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//change to boolean
+dynamicVariable = true
+console.log(dynamicVariable, typeof dynamicVariable);
